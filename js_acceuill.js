e03afa3af06acf0eb64ee429d84843ab0520fd61
AMRAOUI Souaad <140906793+winter892@users.noreply.github.com>
2025-11-26 11:16:54 +0100

    Add files via upload

---
+++ js_acceuill.js
@@ -0,0 +1,65 @@
+
+document.getElementById("searchForm").addEventListener("submit", function(e){
+    const budget = document.getElementById("budget").value;
+    const ville = document.getElementById("ville").value;
+    const type = document.getElementById("type").value;
+    if(!budget || !ville || !type){
+        alert("Veuillez remplir tous les champs !");
+        e.preventDefault();
+    }
+});
+
+// Témoignages
+let index = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+function showTestimonial(i){
+    testimonials.forEach(t => t.style.display = "none");
+    testimonials[i].style.display = "block";
+}
+showTestimonial(index);
+setInterval(() => {
+    index = (index + 1) % testimonials.length;
+    showTestimonial(index);
+}, 5000);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const header = document.querySelector("header"); 
+    const modeBtn = document.createElement("button");
+    modeBtn.id = "darkModeToggle";
+
+    // Icône + texte
+    modeBtn.innerHTML = `<i class="fa-regular fa-moon"></i> `;
+    modeBtn.style.marginLeft = "15px";
+    header.appendChild(modeBtn);
+
+    const logo = document.getElementById("siteLogo");
+
+    modeBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        const isDark = document.body.classList.contains("dark-mode");
+
+        // Icônes professionnelles
+        modeBtn.innerHTML = isDark 
+            ? `<i class="fa-regular fa-sun"></i> `
+            : `<i class="fa-regular fa-moon"></i> `;
+
+        // Logo dynamique
+        logo.src = isDark
+            ? "IMAGES/Logo_mode_dark.png"
+            : "IMAGES/Logo_mode_light.png";
+    });
+});
+
+
+    // ========== MENU BURGER ==========
+    const toggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+
+
+
